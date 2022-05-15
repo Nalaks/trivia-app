cd11673c3b9345ec7061bead9d7e593478aa47b0
Stefan <insertinsight@gmail.com>
2022-05-15 16:40:12 +0200

    added basic structure

---
+++ api/questionApi.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { shuffle } from '../utility/shuffle';
+import { Question } from '../types/types';
+
+export const fetchQuizQuestions = async (amount: number, difficulty: string, category: number) => {
+  let endpoint = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`
+  if (difficulty === 'any' && category === 99) {
+    endpoint = `https://opentdb.com/api.php?amount=${amount}&type=multiple`
+  } else if (category === 99) {
+    endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
+  } else if (difficulty === 'any') {
+    endpoint = `https://opentdb.com/api.php?amount=${amount}&category=${category}&type=multiple`
+  }
+  const res = await axios(endpoint)
+  return res.data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffle([...question.incorrect_answers, question.correct_answer]),
+  }))
+}
